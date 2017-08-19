0b888a12b0ce5b3b661f9eeab6196efc7374b0b3
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-19 18:01:13 +0300

    Add common behaviour for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :resource, :collecion
+
+  def index
+    collection
+  end
+
+  def show
+    resource
+  end
+
+  def create
+    build_resource
+
+    resource.save!
+  end
+
+  def update
+    resource.update! resource_params
+  end
+
+  def destroy
+    resource.destroy!
+  end
 end
