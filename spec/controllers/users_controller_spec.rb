524484327888ec501a776100098f41a6273e87ae
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-19 17:54:50 +0300

    Fix test for UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.decribe Api::UsersController, type: :controller do
+RSpec.describe Api::UsersController, type: :controller do
   it { should be_an ApplicationController }
 
-  decribe '#create' do
-    let(:user_params) { { email: 'test@test.com', password: 'qwerty' } }
+  describe '#create' do
+    let(:user_params) { { email: 'test@test.com', password: 'qwerty', password_confirmation: 'qwerty' } }
 
     let(:params) { { user: user_params } }
 
     let(:user) { stub_model User }
 
-    before { expect(User).to receive(:new).with(permit! params).and_return(user) }
+    before { expect(User).to receive(:new).with(permit! user_params).and_return(user) }
 
     before { expect(user).to receive(:save!) }
 
-    before { process :create, method: :post, params, fromat: :json }
+    before { process :create, method: :post, params: params, format: :json }
 
     it { should render_template :create }
   end
