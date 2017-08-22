b096c341e87a99fec01078cacf205043d4ca875d
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-22 15:21:28 +0300

    Add paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
 
   has_many :auth_tokens, dependent: :destroy
 
+  has_attached_file :avatar, styles: { thumn: "300x300#" } #, default_url: "/images/:style/missing.png"
+
   enum sex: [:male, :female]
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }, email: true
+
+  validates_attachemnt_content_type :avatar, content_type: /\.(png|jpg|jpeg|gif)$/
 end
